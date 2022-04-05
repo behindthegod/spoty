773850524b775dab0073dbf0ed70d0c76aedb6e4
behindthegod <divizionby0@gmail.com>
2022-04-05 22:12:52 +0300

    back

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,13 @@
 import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://Ilya:admin@cluster0.h2eoo.mongodb.net/spoty?retryWrites=true&w=majority'),
+        TrackModule,
 
+    ]
 })
+
 export class AppModule{}
